b57060ac2982829ac54939f41da576d2ad47bbde
DAVID OKEAMAH <whitneykiss741@gmail.com>
2025-01-18 15:50:09 +0100

    Create contract.sol
    Signed-off-by: DAVID OKEAMAH  <whitneykiss741@gmail.com>

---
+++ contract.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+contract Okeamah {
+    // Example contract code
+    uint256 public value;
+
+    function setValue(uint256 newValue) public {
+        value = newValue;
+    }
+}
